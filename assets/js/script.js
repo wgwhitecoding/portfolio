40f8919f988653bfdbf9581b6ff4a249da1235d8
willcodinggod white <walidwhite@live.co.uk>
2024-09-07 21:44:15 +0000

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// Dark Mode Toggle using Font Awesome Icon
+const darkModeToggle = document.getElementById('darkModeToggle');
+const themeIcon = document.getElementById('themeIcon');
+
+darkModeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+
+    // Switch between sun and moon icons
+    if (document.body.classList.contains('dark-mode')) {
+        themeIcon.classList.remove('fa-moon');
+        themeIcon.classList.add('fa-sun');
+    } else {
+        themeIcon.classList.remove('fa-sun');
+        themeIcon.classList.add('fa-moon');
+    }
+});
+
+
+
